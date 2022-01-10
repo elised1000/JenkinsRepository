55cccd745d0f5390b6b5b86dd0e46d7bdc7e1620
elised1000 <95353993+elised1000@users.noreply.github.com>
2022-01-10 14:38:47 +0000

    Update UnisysProgram.java

--- UnisysProgram.java
+++ UnisysProgram.java
@@ -1,6 +1,8 @@
 public class UnisysProgram {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World!");
+		System.out.println("Name: Elise Dunkley");
+		System.out.println("Job Title: Developer");
+		System.out.println("Time in Job Title: 2 years");
 	}
 }
